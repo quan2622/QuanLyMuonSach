fc3049f08d9873c779f404fcf6ca6365987e6e6b
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-03-06 19:44:36 +0700

    init frontend

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>Hello vuejs abc</h1>
+</template>
+
+<style scoped>
+h1 {
+  padding: .2rem 2rem;
+}
+</style>
